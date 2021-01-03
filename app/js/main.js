3f78a5a0da28e04fb21e9965741538d642118053
Sorokin2204 <daniil.sorokin.228@gmail.com>
2021-01-03 17:12:04 +0300

    Add main section of product-one

--- app/js/main.js
+++ app/js/main.js
@@ -21,5 +21,20 @@ $(function () {
     nextArrow: $('.products-slider__arrow-right'),
   });
 
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').slideToggle();
+  });
+
+  $('.product-one__tabs .tab').on('click', function (event) {
+    var id = $(this).attr('data-id');
+    $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+    $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+    $(this).addClass('active');
+    $('#' + id)
+      .addClass('active-tab')
+      .fadeIn();
+    return false;
+  });
+
   var mixer = mixitup('.products__box');
 });
